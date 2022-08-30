4862ba1ded069d50ed624a15250badcf72e0b394
Andre Bauer <andrebauer@live.com>
2022-08-30 12:04:19 +0900

    working on local

--- payload.js
+++ payload.js
@@ -1,27 +1,13 @@
-// send the page title as a chrome message
-// chrome.runtime.sendMessage(
-//   document.querySelector(".top-card-layout__title").innerText
-// );
-// chrome.runtime.sendMessage(
-//   document.querySelector(".topcard__org-name-link").innerText
-// );
-// chrome.runtime.sendMessage(document.URL);
-
-chrome.runtime.sendMessage({
-  content: document.querySelector(
+const job = {
+  title: document.querySelector(
     ".top-card-layout__title, .jobs-unified-top-card__job-title"
   ).innerText,
-  type: "m1",
-});
-chrome.runtime.sendMessage({
-  content: document.querySelector(
+  url: document.URL,
+  company: document.querySelector(
     ".topcard__org-name-link, .jobs-unified-top-card__company-name"
   ).innerText,
-  type: "m2",
+};
+console.log(job);
+chrome.runtime.sendMessage(job, function (response) {
+  console.log(response);
 });
-chrome.runtime.sendMessage({ content: document.URL, type: "m3" });
-chrome.runtime.sendMessage({ content: document, type: "m4" });
-
-//  document.querySelector('.top-card-layout__title').innerText
-//  document.querySelector('.topcard__org-name-link').innerText
-//  document.URL
--- popup.js
+++ popup.js
@@ -4,66 +4,25 @@ window.addEventListener("load", function (evt) {
     file: "payload.js",
   });
 });
-
-// document.getElementById("pagetitle").innerHTML = message;
-
-// Listen to messages from the payload.js script and write to popout.html
+let job;
 chrome.runtime.onMessage.addListener(function (message) {
-  if (message.type == "m1") {
-    document.getElementById("title").innerHTML = message.content;
-  }
-  if (message.type == "m2") {
-    document.getElementById("company").innerHTML = message.content;
-  }
-  if (message.type == "m3") {
-    document.getElementById("url").innerHTML = message.content;
-  }
-
-  // document.getElementById("pagetitle").innerHTML = message;
+  job = message;
+  console.log(message);
 });
 
-// event to on click submit
-// $("#login").click(function () {
-//   var email = $("#email").val();
-//   var password = $("password").val();
-
-//   // Making ajax call after receive
-//   $.post("http://www.jobjob.pro/users/sign_in", {
-//     email: email,
-//     password: password,
-//   });
-//   $.post("http://www.jobjob.pro/jobs", {
-//     title: title,
-//     company: company,
-//     url: url,
-//   });
-// });
-chrome.runtime.onMessage.addListener(function (message) {
-  if (message.type == "m1") {
-    title = message.content;
-  }
-  if (message.type == "m2") {
-    company = message.content;
-  }
-  if (message.type == "m3") {
-    url = message.content;
-  }
-  if (message.type == "m4") {
-    document = message.content;
-  }
-
-  function sendJob() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+function sendJob() {
+  chrome.storage.sync.get(["token"], function (result) {
+    const token = result.token;
+    console.log(token);
     var myInit = {
       method: "POST",
       headers: {
+        Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
-        "X-User-Email": email,
-        "X-User-Password": password,
+        Accept: "application/json",
       },
       body: JSON.stringify({
-        job: { user_id: 5, title: title, company: company, url: url },
+        job: job,
       }),
     };
 
@@ -73,11 +32,48 @@ chrome.runtime.onMessage.addListener(function (message) {
       })
       .then((data) => {
         console.log(data);
+        // todo update display on popup
       });
-  }
-  console.log(document.getElementById("send"));
-  // document.getElementById("send").addEventListener("click", (event) => {
+  });
+}
+
+document.getElementById("send").addEventListener("click", (event) => {
   //   console.log("inside event listener");
   sendJob();
-  // });
+});
+
+function login() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  var myInit = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: email, password: password }),
+  };
+
+  fetch("http://localhost:3000/api/v1/auth/login", myInit)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      chrome.storage.sync.set(data, function () {
+        console.log("Value is set to " + data);
+        document.getElementById("showSignIn").style.display = "none";
+      });
+    });
+}
+
+document.getElementById("login-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("inside event listener");
+  login();
+});
+
+chrome.storage.sync.get(["token"], function (result) {
+  if (result.token) {
+    document.getElementById("showSignIn").style.display = "none";
+  }
 });
