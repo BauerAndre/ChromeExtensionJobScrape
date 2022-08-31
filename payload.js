93d5b4ec25d53f4fd3ed243bf7fd9ce0f8683101
Andre Bauer <andrebauer@live.com>
2022-08-31 11:04:36 +0900

    added logo url to sending

--- payload.js
+++ payload.js
@@ -7,6 +7,7 @@ chrome.runtime.sendMessage(
     company: document.querySelector(
       ".topcard__org-name-link, .jobs-unified-top-card__company-name"
     ).innerText,
+    logo_url: document.querySelector(".lazy-image").innerText,
   },
   function (response) {
     console.log(response);
